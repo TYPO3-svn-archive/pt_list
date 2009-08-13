ecafe717c7387f3137e0bf737b2ec6a53f985964
sonic <sonic@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-13 08:15:40 +0000

    ADD: re-added accidentally removed firstLetter filter alias
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pt_list/trunk@23338 735d13b6-9817-0410-8766-e36946ffe9aa

--- typoscript/static/_default/plugin.tx_ptlist.alias.ts
+++ typoscript/static/_default/plugin.tx_ptlist.alias.ts
@@ -10,6 +10,7 @@ plugin.tx_ptlist.alias {
 	filter_max = EXT:pt_list/controller/filter/class.tx_ptlist_controller_filter_max.php:tx_ptlist_controller_filter_max
     filter_range = EXT:pt_list/controller/filter/class.tx_ptlist_controller_filter_range.php:tx_ptlist_controller_filter_range
     filter_timeSpan = EXT:pt_list/controller/filter/class.tx_ptlist_controller_filter_timeSpan.php:tx_ptlist_controller_filter_timeSpan
+    filter_firstLetter = EXT:pt_list/controller/filter/class.tx_ptlist_controller_filter_firstLetter.php:tx_ptlist_controller_filter_firstLetter
     filter_datePicker = EXT:pt_list/controller/filter/class.tx_ptlist_controller_filter_datePicker.php:tx_ptlist_controller_filter_datePicker
 
     filter_options_group = EXT:pt_list/controller/filter/options/class.tx_ptlist_controller_filter_options_group.php:tx_ptlist_controller_filter_options_group
