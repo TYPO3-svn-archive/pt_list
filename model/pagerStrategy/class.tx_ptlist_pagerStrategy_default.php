5e6c3a7feb0db4605cabdaec1f03d841d12d347e
simonschaufi <simonschaufi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-01 14:07:22 +0000

    ADD: New Pager configurations
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pt_list/trunk@37522 735d13b6-9817-0410-8766-e36946ffe9aa

--- model/pagerStrategy/class.tx_ptlist_pagerStrategy_default.php
+++ model/pagerStrategy/class.tx_ptlist_pagerStrategy_default.php
@@ -26,8 +26,6 @@
 /**
  * Class definition file for a default pager strategy implementation
  * 
- * $Id$
- * 
  * @author Fabrizio Branca <mail@fabrizio-branca.de>
  * @since 2009-01-27
  */
@@ -70,11 +68,9 @@ class tx_ptlist_pagerStrategy_default implements tx_ptlist_iPagerStrategy {
 	protected $amountPages;
 	
 	
-	
 	/***************************************************************************
 	 * Methods implementing the "tx_ptlist_iPagerStrategy" interface
 	 **************************************************************************/
-	
 	
 	
 	/**
@@ -89,7 +85,11 @@ class tx_ptlist_pagerStrategy_default implements tx_ptlist_iPagerStrategy {
 		$this->conf = $conf;
 		
 		// set defaults if no values are configured
-		$this->conf['delta'] = isset($this->conf['delta']) ? $this->conf['delta'] : 0;		
+		$this->conf['fix'] = isset($this->conf['fix']) ? $this->conf['fix'] : 0;      
+		$this->conf['delta'] = isset($this->conf['delta']) ? $this->conf['delta'] : 0;
+		$this->conf['delta'] = $this->conf['fix'] == 0 ? $this->conf['delta'] : 0;      
+		$this->conf['pagesNoFirst'] = isset($this->conf['pagesNoFirst']) ? $this->conf['pagesNoFirst'] : 0;      
+		$this->conf['pagesNoLast'] = isset($this->conf['pagesNoLast']) ? $this->conf['pagesNoLast'] : 0;      
 		$this->conf['elements'] = isset($this->conf['elements']) ? $this->conf['elements'] : 'first, prev, pages, next, last';
 
 		tx_pttools_assert::isValidUid($this->conf['delta'], true, array('message' => 'No valid "delta"!'));
@@ -97,19 +97,17 @@ class tx_ptlist_pagerStrategy_default implements tx_ptlist_iPagerStrategy {
 	}
 	
 	
-	
 	/**
 	 * Sets the current page number
 	 * 
 	 * @param  int  $currentPageNumber  Current Page number
 	 * @return void
-     * @author      Fabrizio Branca <mail@fabrizio-branca.de>
-     * @since       2009-01-27
+	 * @author      Fabrizio Branca <mail@fabrizio-branca.de>
+	 * @since       2009-01-27
 	 */
 	public function setCurrentPageNumber($currentPageNumber) {
 		$this->currentPageNumber = $currentPageNumber;
 	}
-	
 	
 	
 	/**
@@ -117,13 +115,12 @@ class tx_ptlist_pagerStrategy_default implements tx_ptlist_iPagerStrategy {
 	 * 
 	 * @param  int $amountPages    Amount of pages
 	 * @return void
-     * @author      Fabrizio Branca <mail@fabrizio-branca.de>
-     * @since       2009-01-27
+	 * @author      Fabrizio Branca <mail@fabrizio-branca.de>
+	 * @since       2009-01-27
 	 */
 	public function setAmountPages($amountPages) {
 		$this->amountPages = $amountPages;	
 	}
-	
 	
 	
 	/**
@@ -131,30 +128,36 @@ class tx_ptlist_pagerStrategy_default implements tx_ptlist_iPagerStrategy {
 	 * 
 	 * @param  void
 	 * @return void
-     * @author      Fabrizio Branca <mail@fabrizio-branca.de>
-     * @since       2009-01-27
+	 * @author      Fabrizio Branca <mail@fabrizio-branca.de>
+	 * @since       2009-01-27
 	 */
 	public function getLinks() {
 		$links = array();
-		foreach (t3lib_div::trimExplode(',', $this->conf['elements'], true) as $element) {
-			
+
+		foreach (t3lib_div::trimExplode(',', $this->conf['elements']) as $element) {
 			switch ($element) {
 				case 'pages': {		
 					for ($i=1; $i<=$this->amountPages; $i++) {
 						// render the link only if it is in the delta (or if delta is off), if it is the first or the last one
-						if ($this->conf['delta'] == 0 || abs($i-$this->currentPageNumber) <= $this->conf['delta'] || ($i == 1) || ($i == $this->amountPages) ) {
+						if (($this->conf['delta'] == 0 && $this->conf['fix'] == 0)
+							|| (abs($i-$this->currentPageNumber) <= $this->conf['delta'] && $this->conf['delta'] !=0) 
+							|| ($i <= $this->conf['fix'] && $this->currentPageNumber <= $this->conf['fix']   && $this->conf['fix'] !=0)                              
+							|| ($i > $this->currentPageNumber - $this->conf['fix'] && $i <= $this->currentPageNumber && $this->currentPageNumber > $this->conf['fix']  && $this->conf['fix'] !=0)                              
+							|| ($i == 1 && $this->conf['pagesNoFirst'] == false) 
+							|| ($i == $this->amountPages && $this->conf['pagesNoLast'] == false) ) {
 							$links[] = array(
 								'pageNumber' => $i,
 								'label' => $i,
 								'current' => ($i == $this->currentPageNumber),
-								'type' => 'pageitem',
+								'type' => 'pageitem'
 							);
+							#echo '<br>$i.'.$i.' abs:'.abs($this->conf['fix']-$this->currentPageNumber). ' $this->currentPageNumber'.$this->currentPageNumber;
 						} else {
 							// append a "fill" item if it does not already exist
 							$lastLinkItem = end($links);				
 							if ($lastLinkItem['type'] != 'fillitem') {
 								$links[] = array(
-				            		'label' => 'EXT:pt_list/locallang.xml:pager_fill',
+									'label' => 'EXT:pt_list/locallang.xml:pager_fill',
 									'type' => 'fillitem'
 								);
 							}
@@ -165,41 +168,41 @@ class tx_ptlist_pagerStrategy_default implements tx_ptlist_iPagerStrategy {
 				case 'prev': {
 					$prevpage = max($this->currentPageNumber - 1, 1);
 					$links[] =  array(
-		            	'pageNumber' => $prevpage,
-		            	'label' => 'EXT:pt_list/locallang.xml:pager_prev',
+						'pageNumber' => $prevpage,
+						'label' => 'EXT:pt_list/locallang.xml:pager_prev',
 						'current' => ($prevpage == $this->currentPageNumber),
-		            	'type' => 'prev',
-		            );
+						'type' => 'prev'
+					);
 				} break;
 				
 				case 'next': {
 					$nextpage = min($this->currentPageNumber + 1, $this->amountPages);
-            		$links[] = array(
-	            		'pageNumber' => $nextpage,
-	            		'label' => 'EXT:pt_list/locallang.xml:pager_next',
+					$links[] = array(
+						'pageNumber' => $nextpage,
+						'label' => 'EXT:pt_list/locallang.xml:pager_next',
 						'current' => ($nextpage == $this->currentPageNumber),
-	            		'type' => 'next',
-	            	);
+						'type' => 'next'
+					);
 				} break; 
 				
 				case 'first': {
 					$firstpage = 1;
-			        $links[] = array(
-		            	'pageNumber' => $firstpage,
-		            	'label' => 'EXT:pt_list/locallang.xml:pager_first',
+					$links[] = array(
+						'pageNumber' => $firstpage,
+						'label' => 'EXT:pt_list/locallang.xml:pager_first',
 						'current' => ($firstpage == $this->currentPageNumber),
-		            	'type' => 'first',
-		            );
+						'type' => 'first'
+					);
 				} break;
 				
 				case 'last': {
-            		$lastpage = $this->amountPages;
-			        $links[] = array(
-		            	'pageNumber' => $lastpage,
-		            	'label' => 'EXT:pt_list/locallang.xml:pager_last',
+					$lastpage = $this->amountPages;
+					$links[] = array(
+						'pageNumber' => $lastpage,
+						'label' => 'EXT:pt_list/locallang.xml:pager_last',
 						'current' => ($lastpage == $this->currentPageNumber),
-		            	'type' => 'last',
-		            );
+						'type' => 'last'
+					);
 					
 				} break;
 
@@ -207,9 +210,9 @@ class tx_ptlist_pagerStrategy_default implements tx_ptlist_iPagerStrategy {
 					// nothing to be done here, except for appending a page element with the "offsetinfo" type
 					// offset info data is available in the view under the keys offSetStart, offSetEnd, totalItemCount
 					// this page element acts only as a placeholder...
-			        $links[] = array(
-		            	'type' => 'offsetinfo',
-		            );
+					$links[] = array(
+						'type' => 'offsetinfo'
+					);
 
 				} break;
 				
